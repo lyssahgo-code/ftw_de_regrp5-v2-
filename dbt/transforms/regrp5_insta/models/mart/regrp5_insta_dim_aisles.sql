79bc44054f7db3e830901a74bdfafc58a93a221a
ftw-zlarandela <zushiela.arandela@ftwfoundation.org>
2025-10-11 09:26:05 +0800

    -Add Mart sql files and update sources.yml

--- dbt/transforms/regrp5_insta/models/mart/regrp5_insta_dim_aisles.sql
+++ dbt/transforms/regrp5_insta/models/mart/regrp5_insta_dim_aisles.sql
@@ -4,10 +4,4 @@ select
   aisle_id as AisleKey,
   aisle
 from {{ source('clean', 'regrp5_insta_aisle') }}
-ORDER BY aisle_id{{ config(materialized="table", schema="mart", tags=["dimension","regrp5"], cluster_by=["AisleKey"]) }}
-
-select
-  aisle_id as AisleKey,
-  aisle
-from {{ source('clean', 'regrp5_insta_aisle') }}
 ORDER BY aisle_id
